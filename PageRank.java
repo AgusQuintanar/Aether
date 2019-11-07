0df2578ad707b3b93c310c323bec0371639b5a25
AgusQuintanar <agusquintanar17@gmail.com>
2019-11-06 18:15:57 -0600

    Added random tests to page rank

--- PageRank.java
+++ PageRank.java
@@ -1,7 +1,6 @@
 import java.util.HashMap;
-
 import Jama.Matrix;
-
+import java.util.Random;
 
 public class PageRank {
     private Website[] websites;
@@ -22,8 +21,10 @@ public class PageRank {
                 else this.H[j][i] = 0;
             }
         }
-
+        this.printMatrix(this.H);
         this.H = new Matrix(this.H).inverse().getArray();
+
+        this.printMatrix(this.H);
 
         for (int i=0; i<this.websites.length; i++) {
             double rankI = 0;
@@ -34,19 +35,49 @@ public class PageRank {
         }
     }
 
+    private void printMatrix(double[][] matrix) {
+        for (int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ,");
+            } System.out.println();
+        }
+    }
+
+
 
     public static void main(String[] args) {
-        HashMap<Integer,Integer> B1 = new HashMap<>(),B2 = new HashMap<>(),B3 = new HashMap<>(),B4 = new HashMap<>(),B5 = new HashMap<>();
+        // HashMap<Integer,Integer> B1 = new HashMap<>(),B2 = new HashMap<>(),B3 = new HashMap<>(),B4 = new HashMap<>(),B5 = new HashMap<>();
 
-        B2.put(0, 1);
-        B3.put(1, 1);
-        B4.put(4, 1);
-        B5.put(0, 1);
-        B5.put(1, 1);
-        B5.put(2, 1);
-        B5.put(3, 1);
+        // B2.put(0, 1);
+        // B3.put(1, 1);
+        // B4.put(4, 1);
+        // B5.put(0, 1);
+        // B5.put(1, 1);
+        // B5.put(2, 1);
+        // B5.put(3, 1);
 
-        Website[] websites = {new Website(2, B1,0),new Website(2, B2,1),new Website(1, B3,2),new Website(1, B4,3),new Website(1, B5,4)};
+        // Website[] websites = {new Website(2, B1,0),new Website(2, B2,1),new Website(1, B3,2),new Website(1, B4,3),new Website(1, B5,4)};
+        int init_size = 10000;
+        Website[] websites = new Website[init_size];
+        Random r = new Random();
+
+        for(int i=0; i < init_size; i++){
+            HashMap<Integer,Integer> Bi = new HashMap<>();
+            int bSize = r.nextInt(init_size/2);
+            System.out.println("bsize: "+bSize);
+            for (int j=0; j < bSize; j++) {
+                int id_link_from = r.nextInt(init_size);
+                //System.out.println("id ext: " + id_link_from);
+                Bi.put(id_link_from, 1);
+            }
+            int random_l = r.nextInt(init_size);
+            //System.out.println("random l: "+random_l);
+            websites[i] = new Website(random_l,Bi,i);
+            System.out.println("-----------------------------------");
+        }
+
+
+
         PageRank pg = new PageRank(websites);
         pg.calculateRanks();
 
