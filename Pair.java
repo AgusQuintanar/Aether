444900ba298293374c8970854f2b78f5c72228f2
Jonathan Chávez <jonathanchatab@hotmail.com>
2019-11-17 21:04:48 -0600

    Add files via upload

---
+++ Pair.java
@@ -0,0 +1,9 @@
+public class Pair<E, T>{
+	public E first;
+	public T second;
+	
+	Pair(E first, T second){
+		this.first = first;
+		this.second = second;
+	}
+}
--- Website.java
+++ Website.java
@@ -1,17 +1,27 @@
 //Jonathan Chavez A01636160
+//Agustin Quintanar A01636142
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 
 public class Website {
 	
-	private String url;
-	private String title;
-	private String metaDescription;
-	private String keywords[];
+	private String url,
+				   title,
+				   metaDescription,
+				   rawHtml;
+
+	private String[] keywords;
 	
-	
-	private String rawHtml;
+	private ArrayList<String> linksTo = new ArrayList<String>();
+
 	private int visitors = 0;
+
 	private Date created;
+				 
+	private double rank; 
+
 	
 	
 	Website(String url, String rawHtml){
@@ -19,9 +29,11 @@ public class Website {
 		this.rawHtml = rawHtml;
 		this.parseHtml();
 		this.created = new Date();
+		this.rank = 0;
 	}
+
 	
-	//Método auxiliar que toma el html del objeto y asigna las propiedades [metaTags, metaDescription, title]
+	//Auxiliar method called in constructor that parses the HTML of the object and sets the properties [metaTags, metaDescription, title, outside_links]
 	private void parseHtml() {
 		
 		//Parsing title
@@ -44,6 +56,36 @@ public class Website {
 			keywordsClosingTagIndex = i;
 		}
 		this.keywords = rawHtml.substring(keywordsOpeningTagIndex, keywordsClosingTagIndex+1).split(",");
+		
+		
+		//Parsing outside links
+		HashSet<String> uniqueLinks = new HashSet<String>();
+		int beginIndex = 0;
+		
+		while(beginIndex != -1) {
+			int linkOpeningTagIndex = this.rawHtml.indexOf("<a href=\"", beginIndex);
+			
+			if(linkOpeningTagIndex == -1) {
+				break;
+			} else {
+				linkOpeningTagIndex += 9; // Adds the size of "<a href=" string
+			}
+			int linkClosingTagIndex = this.rawHtml.indexOf("\"", linkOpeningTagIndex);
+		
+			beginIndex = linkClosingTagIndex;
+			
+			//Trims subpages and only leaves domain name
+			String link = rawHtml.substring(linkOpeningTagIndex, linkClosingTagIndex);
+			int idxFirstSlash = link.indexOf('/', 8);
+			if(idxFirstSlash != -1) {
+				link = link.substring(0, idxFirstSlash);
+			}
+			
+			if(!uniqueLinks.contains(link)) {
+				uniqueLinks.add(link);
+				this.linksTo.add(link);
+			}
+		}
 	}
 	
 	public String toString() {
@@ -65,6 +107,13 @@ public class Website {
 		res += "VISITORS: "+this.visitors+"\n";
 		
 		res += "CREATED: "+this.created+"\n";
+		
+		res += "THIS PAGE POINTS TO: ";
+		for(int i=0; i<this.linksTo.size(); i++) {
+			res += this.linksTo.get(i);
+			if(i != this.linksTo.size()-1) res+= ", ";
+		}
+		res += "\n";
 		
 		return res;
 	}
@@ -93,13 +142,29 @@ public class Website {
 		return created;
 	}
 
+	public void setRank(double rank) {
+		this.rank = rank;
+	}
+
+	public double getRank() {
+		return this.rank;
+	}
+	
+	public String[] getKeywords() {
+		return this.keywords;
+	}
+	
+	public ArrayList<String> getLinksTo() {
+		return this.linksTo;
+	}
+
 	public static void main(String[] args) {
-		Website trivago = new Website("trivago.com", "<html><head><title>Trivago -  El mejor lugar para tu viaje</title><meta charset=\"UTF-8\">\n" + 
-				"  <meta name=\"description\" content=\"Free Web tutorials\">\n" + 
-				"  <meta name=\"keywords\" content=\"HTML,CSS,XML,JavaScript\">\n" + 
-				"  <meta name=\"author\" content=\"John Doe\">\n" + 
-				"  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"></head><body><h1>Encuentra hoteles aquí</h1></body></html>");
-		System.out.println(trivago);
+		 Website trivago = new Website("trivago.com", "<html><head><title>Trivago -  El mejor lugar para tu viaje</title><meta charset=\"UTF-8\">\n" + 
+		 		"  <meta name=\"description\" content=\"Free Web tutorials\">\n" + 
+		 		"  <meta name=\"keywords\" content=\"HTML,CSS,XML,JavaScript\">\n" + 
+		 		"  <meta name=\"author\" content=\"John Doe\">\n" + 
+		 		"  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"></head><body><h1>Encuentra hoteles <a href=\"https://hoteles.com\">aquí</a> y <a href=\"https://hola.com\"></a>allá <a href=\"https://hola.com/caca\"></a></h1></body></html>");
+		 System.out.println(trivago);
 	}
 	
 	
