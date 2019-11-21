23baf2b56fc7a00c2afdae45ada82274844afc4a
AgusQuintanar <agusquintanar17@gmail.com>
2019-11-20 22:26:02 -0600

    Aether search now works :)

--- BACKEND/Aether.java
+++ BACKEND/Aether.java
@@ -13,9 +13,6 @@ public class Aether {
         this.pageRank = new PageRank(this.index.getCount());
     }
 
-    // public Keywords getKeywords() {
-    //     return this.keywords;
-    // }
 
     public Index getIndex() {
         return this.index;
@@ -25,32 +22,40 @@ public class Aether {
         return this.keywords;
     }
 
+    public SearchResults getSearchResults() {
+        return this.searchResults;
+    }
+
     public void updateWebsitesRanks() {
         Website[] websitesWithNewRanks = this.pageRank.calculateRanks(this.index);
         this.index.setWebsites(websitesWithNewRanks);
         this.index.writeMetadata();
     }
 
-    // private void search(String searchPhrase) {
-    //     SearchPhrase sf = new SearchPhrase(searchPhrase);
-    //     HashMap<Website, Double> websitesFound = new HashMap<>();
-        
-    //     for (String comb : sf.getComninations()) {
-    //         if (this.keywords.containsKey(comb)) {
-    //             for (Website wsFound : this.keywords.get(comb)) {
-    //                 if (websitesFound.containsKey(wsFound)) {
-    //                     double oldRank = websitesFound.get(wsFound);
-    //                     websitesFound.replace(wsFound, oldRank, 2*oldRank);
-    //                 }
-    //                 else {
-    //                     websitesFound.put(wsFound, Math.pow(2,comb.length())*(0.85*wsFound.getRank()+0.15*wsFound.getVisitors()));
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     this.searchResults = new SearchResults(websitesFound);
+    public void updateKeywordsFromWebsites() {
+        this.keywords.updateKeywordsFromWebsites(this.index);
+    }
 
-    // }
+    private void search(String searchPhrase) {
+        SearchPhrase sf = new SearchPhrase(searchPhrase);
+        HashMap<Website, Double> websitesFound = new HashMap<>();
+        
+        for (String comb : sf.getComninations()) {
+            if (this.keywords.containsKey(comb)) {
+                for (Website wsFound : this.keywords.get(comb)) {
+                    if (websitesFound.containsKey(wsFound)) {
+                        double oldRank = websitesFound.get(wsFound);
+                        websitesFound.replace(wsFound, oldRank, 2*oldRank);
+                    }
+                    else {
+                        websitesFound.put(wsFound, Math.pow(2,comb.length())*(0.85*wsFound.getRank()+0.15*wsFound.getVisitors()));
+                    }
+                }
+            }
+        }
+        this.searchResults = new SearchResults(websitesFound);
+
+    }
 
     public static void main(String[] args) {
         Aether aether = new Aether();
@@ -58,7 +63,14 @@ public class Aether {
 
         //for (int i=0; i<aether.getIndex().getCount(); i++) System.out.println(aether.getIndex().getWebsites()[i].getUrl()+", ");
 
-        System.out.println(aether.getKeywords());
+        //aether.updateKeywordsFromWebsites();
+
+
+        System.out.println("Resultados de la busqueda");
+        aether.search("perros sin camisa");
+
+        for (SearchResult searchResult : aether.getSearchResults().getSearchResults()) System.out.println(searchResult + ", ");
+        
     }
 
 }
--- BACKEND/Keywords.java
+++ BACKEND/Keywords.java
@@ -14,37 +14,59 @@ public class Keywords extends HashMap<String,LinkedList<Website>>{
     public Keywords(Index index) {
         super(100000); //Initial keyword size
         this.loadKeywords(index);
+        this.writeKeywords();
     }
     
+    public void updateKeywordsFromWebsites(Index index) {
+        this.clear();
+        this.clearKeywordsTXT();
+        this.loadKeywordsFromWebsites(index);
+        this.writeKeywords();
+    }
+
     public void loadKeywords(Index index) {
         this.loadKeywordsFromKeywordsTXT(index);
-        this.loadKeywordsFromWebsites(index);
+    }
+
+    private void clearKeywordsTXT() {
+        try {
+            FileWriter fw = new FileWriter(pathFile);
+            PrintWriter pw = new PrintWriter(fw);
+            pw.println("");
+            pw.close();
+          }
+        catch(IOException ex) {
+            System.out.println(pathFile+" can not be accessed.");
+        }
     }
 
     private void loadKeywordsFromWebsites(Index index) {
-        try {
+      
+
             for (Website websiteInKeyword : index.getWebsites()) { 
 
-                String keywordsString = "";
-                for (String keyword : websiteInKeyword.getKeywords()) keywordsString+=keyword+",";
-
-                System.out.println("Website: "+websiteInKeyword+", Keywords: "+keywordsString);
-                for (String keyword : websiteInKeyword.getKeywords()) {
-                    if (this.containsKey(keyword)) { //If the keyword is already in Keywords hashmap
-                        LinkedList<Website> oldWebsites = this.get(keyword);
-                        oldWebsites.add(websiteInKeyword);
-                        this.replace(keyword, oldWebsites);
-                    }
-                    else { //If the keyword is not in Keywords hashmap
-                        LinkedList<Website> newKeywordWebsites = new LinkedList<>();
-                        newKeywordWebsites.add(websiteInKeyword);
-                        this.put(keyword, newKeywordWebsites);
+                if (websiteInKeyword != null) {
+                    for (String keyword : websiteInKeyword.getKeywords()) {
+                        if (keyword != null) {
+                            if (this.containsKey(keyword)) { //If the keyword is already in Keywords hashmap
+                                LinkedList<Website> oldWebsites = this.get(keyword),
+                                                    newWebsites = oldWebsites;
+                                newWebsites.add(websiteInKeyword);
+                                this.replace(keyword, oldWebsites, newWebsites);
+        
+                            }
+                            else { //If the keyword is not in Keywords hashmap
+                                LinkedList<Website> newKeywordWebsites = new LinkedList<>();
+                                newKeywordWebsites.add(websiteInKeyword);
+                                this.put(keyword, newKeywordWebsites);
+                            }
+                        }
+                    
                     }
                 }
+                
             }
-        } catch (NullPointerException npe) {
-            System.out.println("There are no websites in the Index OR there are no keywords in a website");
-        }
+       
         
     }
 
@@ -83,7 +105,7 @@ public class Keywords extends HashMap<String,LinkedList<Website>>{
             this.forEach((keyword, websites) -> {
                 String urlsString = "";
                 for (Website ws : websites) urlsString += ws.getUrl() + ",";
-                pw.println(keyword+"---"+urlsString);
+                pw.println(keyword.trim().toLowerCase()+"---"+urlsString.trim().toLowerCase());
             });
             pw.close();
             return true;
