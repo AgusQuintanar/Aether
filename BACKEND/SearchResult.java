5aa3f2a21f26d8b30368cf34128e47b8a2d11ec8
AgusQuintanar <agusquintanar17@gmail.com>
2019-11-21 10:40:12 -0600

    Fixed public url in search result class

--- BACKEND/SearchResult.java
+++ BACKEND/SearchResult.java
@@ -22,6 +22,6 @@ public class SearchResult implements Comparable<SearchResult>{
     }
 
     public String toString() {
-        return this.website.getUrl();
+        return this.website.getPublicUrl();
     }
 }
